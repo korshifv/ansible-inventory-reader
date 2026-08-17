fb7aee3a1a2e0b1a84b9e7cde5957603558454e8
Korshi <kerriganfv@gmail.com>
2026-08-17 17:41:48 +0300

    Add application entry point

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include "inventorydocument.h"
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    QGuiApplication::setOrganizationName(QStringLiteral("Korshi"));
+    QGuiApplication::setApplicationName(QStringLiteral("Ansible Inventory Studio"));
+
+    InventoryDocument inventory;
+
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty(QStringLiteral("inventory"), &inventory);
+    engine.rootContext()->setContextProperty(QStringLiteral("inventoryTreeModel"), inventory.treeModel());
+
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+                     &app, [] { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
+
+    engine.loadFromModule(QStringLiteral("AnsibleInventoryStudio"), QStringLiteral("Main"));
+
+    if (argc > 1)
+        inventory.openFile(QUrl::fromLocalFile(QString::fromLocal8Bit(argv[1])));
+
+    return app.exec();
+}
